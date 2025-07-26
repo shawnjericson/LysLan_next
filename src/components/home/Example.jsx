8782b0185962f3aef57b09efedda7c2e3e75b165
shawnjericson <shawnjericson@gmail.com>
2025-07-26 20:11:46 +0700

    Create Example.jsx

---
+++ src/components/home/Example.jsx
@@ -0,0 +1,24 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { supabase } from '@/lib/supabaseClient'
+
+export default function Home() {
+  const [data, setData] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from('your_table_name').select('*')
+      if (error) console.error('Error fetching:', error)
+      else setData(data)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      <h1>Dữ liệu từ Supabase</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  )
+}
